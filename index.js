39f1ab75973c8ff1cf123a660e0db480d84786c8
jessicasewell <sewell.jessica95@gmail.com>
2021-04-11 22:15:08 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity2= "go to the office") {
+    return `This Monday, I will ${activity2}.`;
+}
+function wrapAdjective(str = "*") {
+
+    return function inner(word = 'special') 
+    {
+        word = str + word + str + '!'
+        return `You are ${word}` 
+    }
+
+}
